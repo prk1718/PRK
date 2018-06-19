cc83c8596a426945cf697b2d358759cc9af12439
RW91 <prkjavaee@gmail.com>
2018-06-19 21:10:36 +0200

    dokumentacja

--- src/application/ControlerClient.java
+++ src/application/ControlerClient.java
@@ -21,6 +21,10 @@ import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 
+/**
+ * @author Seweryn Czapiewski and Rafał Witkowski
+ * This class is created for handling all actions both from client and user perspective
+ **/
 public class ControlerClient {
 
 	private Server server;
@@ -60,6 +64,10 @@ public class ControlerClient {
 	@FXML
 	private CheckBox orientacja;
 
+	/**
+	 * @author Seweryn Czapiewski and Rafał Witkowski
+	 * Initialize method set all parameters and gui before showing client and user final gui client
+	 */
 	@FXML
 	private void initialize() {
 		initcheckBoxArrayList();
@@ -84,6 +92,10 @@ public class ControlerClient {
 
 	}
 
+	/**
+	 * @author Rafał Witkowski
+	 * initcheckBoxArrayList method add all checkboxes for ships to Arraylist of checkboxes
+	 */
 	private void initcheckBoxArrayList() {
 		checkBoxArrayList.add(jednoM);
 		checkBoxArrayList.add(dwuM);
@@ -91,6 +103,11 @@ public class ControlerClient {
 		checkBoxArrayList.add(czteroM);
 	}
 
+	/**
+	 * @return string value of selected checkbox of ship
+	 * @author Rafał Witkowski
+	 * getSelectedCheckboxValue is used to get value of selected checkbox
+	 */
 	private String getSelectedCheckboxValue() {
 		String selectedCheckbox = "";
 		for (CheckBox checkBox : checkBoxArrayList) {
@@ -101,6 +118,10 @@ public class ControlerClient {
 		return selectedCheckbox;
 	}
 
+	/**
+	 * @author Seweryn Czapiewski
+	 * initButtonView method set default graphic images for all fields both in user and enemy board
+	 */
 	private void initButtonView() {
 		for (Node node : mojGrid.getChildren()) {
 			setImageAsBackground(node);
@@ -110,6 +131,11 @@ public class ControlerClient {
 		}
 	}
 
+	/**
+	 * @author Rafał Witkowski
+	 * setImageAsBackground method is used by initButtonView to set default field view
+	 * @see #initButtonView()
+	 */
 	private void setImageAsBackground(Node node) {
 		Button button = (Button) node;
 		button.setStyle("-fx-border-style: none; -fx-border-width: 0px; -fx-border-insets: 0; -fx-font-size:1px; -fx-background-image: url('button.jpg')");
@@ -118,6 +144,14 @@ public class ControlerClient {
 
 	}
 
+	/**
+	 * @param isApplicationAServer defines if running app is a server app
+	 * @param port                 defines port for connection
+	 * @param ipAdress             defines IP adress for connection
+	 * @author Seweryn Czapiewski
+	 * setServerClient method creates proper view for selected option at the beginning of aplication
+	 * @see #initButtonView()
+	 */
 	public void setServerClient(boolean isApplicationAServer, String port, String ipAdress) {
 		isAppServer = isApplicationAServer;
 
@@ -128,6 +162,10 @@ public class ControlerClient {
 		}
 	}
 
+	/**
+	 * @author Seweryn Czapiewski
+	 * sendMessageButtonAction method sends message to chat for another running application
+	 */
 	@FXML
 	public void sendMessageButtonAction() {
 		String nick = nickTextField.getText();
@@ -144,11 +182,19 @@ public class ControlerClient {
 		}
 	}
 
+	/**
+	 * @author Rafał Witkowski
+	 * wyczyscButtonAction method clears chat message area
+	 */
 	@FXML
 	public void wyczyscButtonAction() {
 		texMessResive.setText("");
 	}
 
+	/**
+	 * @author Seweryn Czapiewski
+	 * klikGridMojGetRowCol method set clicked field in player board for private field in ControlerClient class
+	 */
 	public void klikGridMojGetRowCol() {
 
 		ustawIndexyGridMoj();
@@ -156,6 +202,14 @@ public class ControlerClient {
 		setPickedFieldIndex(mojGrid, getRowColMoj);
 	}
 
+	/**
+	 * @param mojGrid      is a player board
+	 * @param getRowColMoj is a string array for ControlerClient usage
+	 * @author Seweryn Czapiewski
+	 * setPickedFieldIndex method set clicked field for private field - String array in ControlerClient class<br>
+	 * Array in index of 0 represents a row of clicked field<br>
+	 * Array in index of 1 represents a column of clicked field<br>
+	 */
 	private void setPickedFieldIndex(GridPane mojGrid, String[] getRowColMoj) {
 		mojGrid.addEventFilter(MouseEvent.MOUSE_PRESSED, e -> {
 
@@ -172,6 +226,10 @@ public class ControlerClient {
 		});
 	}
 
+	/**
+	 * @author Seweryn Czapiewski
+	 * klikGridPrzeciwnikGetRowCol method set clicked field in enemy board for private field in ControlerClient class
+	 */
 	public void klikGridPrzeciwnikGetRowCol() {
 
 		ustawIndexyGridPrzeciwnik();
@@ -179,7 +237,14 @@ public class ControlerClient {
 		setPickedFieldIndex(przeciwnikGrid, getRowColPrzeciwnik);
 	}
 
-
+	/**
+	 * @param row      row of field in board
+	 * @param col      column of field in board
+	 * @param gridPane board - enemies or players
+	 * @return Node which is a superclass for Button
+	 * @author Seweryn Czapiewski
+	 * getButtonByRowColumnIndex method return clicked Node(Button)
+	 */
 	public Node getButtonByRowColumnIndex(final int row, final int column, GridPane gridPane) {
 		Node result = null;
 		ObservableList<Node> childrens = gridPane.getChildren();
@@ -195,14 +260,28 @@ public class ControlerClient {
 		return result;
 	}
 
+	/**
+	 * @author Seweryn Czapiewski
+	 * ustawIndexyGridMoj method sets indexes for all player board fields
+	 */
 	public void ustawIndexyGridMoj() {
 		setProperIndexForGrid(mojGrid);
 	}
 
+	/**
+	 * @author Seweryn Czapiewski
+	 * ustawIndexyGridPrzeciwnik method sets indexes for all enemy board fields
+	 */
 	public void ustawIndexyGridPrzeciwnik() {
 		setProperIndexForGrid(przeciwnikGrid);
 	}
 
+	/**
+	 * @author Seweryn Czapiewski
+	 * setProperIndexForGrid method sets indexes for all board fields
+	 * @see #ustawIndexyGridPrzeciwnik()
+	 * @see #ustawIndexyGridMoj()
+	 */
 	private void setProperIndexForGrid(GridPane przeciwnikGrid) {
 		for (int i = 0; i <= 9; i++) {
 
@@ -214,30 +293,54 @@ public class ControlerClient {
 		}
 	}
 
+	/**
+	 * @author Rafał Witkowski
+	 * akcjaCheck4 method unset all selected ship checkboxes and set 4field checbox selected
+	 * @see #setRadioButtonsNotSelected()
+	 */
 	@FXML
 	public void akcjaCheck4() {
 		setRadioButtonsNotSelected();
 		czteroM.setSelected(true);
 	}
 
+	/**
+	 * @author Rafał Witkowski
+	 * akcjaCheck3 method unset all selected ship checkboxes and set 3field checbox selected
+	 * @see #setRadioButtonsNotSelected()
+	 */
 	@FXML
 	public void akcjaCheck3() {
 		setRadioButtonsNotSelected();
 		trzyM.setSelected(true);
 	}
 
+	/**
+	 * @author Rafał Witkowski
+	 * akcjaCheck2 method unset all selected ship checkboxes and set 2field checbox selected
+	 * @see #setRadioButtonsNotSelected()
+	 */
 	@FXML
 	public void akcjaCheck2() {
 		setRadioButtonsNotSelected();
 		dwuM.setSelected(true);
 	}
 
+	/**
+	 * @author Rafał Witkowski
+	 * akcjaCheck1 method unset all selected ship checkboxes and set 1field checbox selected
+	 * @see #setRadioButtonsNotSelected()
+	 */
 	@FXML
 	public void akcjaCheck1() {
 		setRadioButtonsNotSelected();
 		jednoM.setSelected(true);
 	}
 
+	/**
+	 * @author Rafał Witkowski
+	 * setRadioButtonsNotSelected method unset all selected ship checkboxes
+	 */
 	private void setRadioButtonsNotSelected() {
 		jednoM.setSelected(false);
 		dwuM.setSelected(false);
@@ -245,6 +348,10 @@ public class ControlerClient {
 		czteroM.setSelected(false);
 	}
 
+	/**
+	 * @author Rafał Witkowski and Seweryn Czapiewski
+	 * akcjaWstaw method is used to place a selected ship on a players board
+	 */
 	@FXML
 	public void akcjaWstaw() {
 		String selectedShipModel = getSelectedCheckboxValue();
@@ -303,6 +410,12 @@ public class ControlerClient {
 		setProperTextForCheckbox();
 	}
 
+	/**
+	 * @param row row of a selected field
+	 * @param col column of a selected field
+	 * @author Rafał Witkowski
+	 * setNotPossibleImageForFields method set graphic image for not possible ship placement on a players board in nearby of selected field
+	 */
 	private void setNotPossibleImageForFields(int row, int col) {
 		for (int i = row - 1; i <= row + 1; i++) {
 			for (int j = col - 1; j <= col + 1; j++) {
@@ -318,13 +431,21 @@ public class ControlerClient {
 		}
 	}
 
-
+	/**
+	 * @param button field of ship placement
+	 * @author Rafał Witkowski
+	 * setDisplayForPlacedShip method set graphic image for possible ship placed on players board
+	 */
 	private void setDisplayForPlacedShip(Button button) {
 		button.setStyle("-fx-border-style: none; -fx-border-width: 0px; -fx-border-insets: 0; -fx-font-size:1px; -fx-background-image: url('button.jpg')");
 		Image image = new Image(getClass().getResourceAsStream("../view/ship.jpg"));
 		button.setGraphic(new ImageView(image));
 	}
 
+	/**
+	 * @author Seweryn Czapiewski and Rafał Witkowski
+	 * akcjaUsun method handles action of removing placed ship from players board
+	 */
 	@FXML
 	public void akcjaUsun() {
 		if ((!getRowColMoj[0].equals("") && !getRowColMoj[1].equals(""))) {
@@ -365,6 +486,12 @@ public class ControlerClient {
 		setProperTextForCheckbox();
 	}
 
+	/**
+	 * @author Rafał Witkowski
+	 * renderFieldsNotPossibleForPlacingShip method refresh a players board after adding or removing ship from players board
+	 * @see #akcjaUsun()
+	 * @see #akcjaWstaw()
+	 */
 	private void renderFieldsNotPossibleForPlacingShip() {
 		//RW: tu muszą być dwie oddzielne pętle
 		for (Node node : mojGrid.getChildren()) {
@@ -386,6 +513,12 @@ public class ControlerClient {
 		}
 	}
 
+	/**
+	 * @author Rafał Witkowski
+	 * setProperTextForCheckbox method refresh a count of left ships to place by their type, refreshes after adding or removing ship
+	 * @see #akcjaUsun()
+	 * @see #akcjaWstaw()
+	 */
 	private void setProperTextForCheckbox() {
 		jednoM.setText("1 (" + (statkiMap.get("1") - statkiPlacedMap.get("1")) + ")");
 		dwuM.setText("2 (" + (statkiMap.get("2") - statkiPlacedMap.get("2")) + ")");
@@ -393,6 +526,10 @@ public class ControlerClient {
 		czteroM.setText("4 (" + (statkiMap.get("4") - statkiPlacedMap.get("4")) + ")");
 	}
 
+	/**
+	 * @author Seweryn Czapiewski
+	 * akcjaStrzelaj method handles shooting action to an enemy board
+	 */
 	@FXML
 	public void akcjaStrzelaj() {
 		String message = "#strzal#" + getRowColPrzeciwnik[0] + "," + getRowColPrzeciwnik[1] + "$strzal$";
@@ -412,6 +549,11 @@ public class ControlerClient {
 		}
 	}
 
+	/**
+	 * @param mess message send to another application
+	 * @author Seweryn Czapiewski
+	 * weryfikacja method check whether picked field in enemy board contained ship or shoot was a miss
+	 */
 	public void weryfikacja(String mess, TextArea text) {
 
 		if (mess.indexOf("#mess#") != -1 && mess.indexOf("$mess$") != -1) {
@@ -443,7 +585,6 @@ public class ControlerClient {
 						((Button) getButtonByRowColumnIndex(Integer.parseInt(messZapas[0]), Integer.parseInt(messZapas[1]), mojGrid)).setBackground(new Background(new BackgroundFill(
 								Color.LIGHTGREY, null, null)));
 						((Button) getButtonByRowColumnIndex(Integer.parseInt(messZapas[0]), Integer.parseInt(messZapas[1]), mojGrid)).setText("P");
-
 
 						String mess = "#pudlo#" + messZapas[0] + "," + messZapas[1] + "$pudlo$";
 						if (isAppServer) {
@@ -481,6 +622,12 @@ public class ControlerClient {
 		}
 	}
 
+	/**
+	 * @param colMess column of field which was shot
+	 * @param rowMess row of field which was shot
+	 * @author Rafał Witkowski
+	 * setImageEnemyShipNotHit method set proper graphic for misses shoot at enemies board
+	 */
 	private void setImageEnemyShipNotHit(String rowMess, String colMess) {
 		Integer row = Integer.valueOf(rowMess);
 		Integer col = Integer.valueOf(colMess);
@@ -490,6 +637,12 @@ public class ControlerClient {
 		button.setGraphic(new ImageView(image));
 	}
 
+	/**
+	 * @param colMess column of field which was shot
+	 * @param rowMess row of field which was shot
+	 * @author Rafał Witkowski
+	 * setImageEnemyShipNotHit method set proper graphic for hit shoot at enemies board
+	 */
 	private void setImageEnemyShipHit(String rowMess, String colMess) {
 		Integer row = Integer.valueOf(rowMess);
 		Integer col = Integer.valueOf(colMess);
@@ -499,6 +652,13 @@ public class ControlerClient {
 		button.setGraphic(new ImageView(image));
 	}
 
+	/**
+	 * @author Rafał Witkowski
+	 * initStatkiPlacedMap method crates HashMap of ships where key is a type of ship and value is number of currenty placed ships by type<br>
+	 * updates on action add and remove ships
+	 * @see #akcjaUsun()
+	 * @see #akcjaWstaw()
+	 */
 	private void initStatkiPlacedMap() {
 		statkiPlacedMap.put("1", 0);
 		statkiPlacedMap.put("2", 0);
@@ -506,6 +666,13 @@ public class ControlerClient {
 		statkiPlacedMap.put("4", 0);
 	}
 
+	/**
+	 * @author Rafał Witkowski
+	 * initStatkiMap method crates HashMap of ships where key is a type of ship and value is number of ships to be placed at the beginning by type<br>
+	 * updates on action add and remove ships
+	 * @see #akcjaUsun()
+	 * @see #akcjaWstaw()
+	 */
 	private void initStatkiMap() {
 		statkiMap.put("1", 4);
 		statkiMap.put("2", 3);
@@ -513,12 +680,24 @@ public class ControlerClient {
 		statkiMap.put("4", 1);
 	}
 
+	/**
+	 * @param button field for setting default graphic
+	 * @author Rafał Witkowski
+	 * setDefaultBackgroundForField method sets a default graphic for a field
+	 * updates on action add and remove ships
+	 */
 	private void setDefaultBackgroundForField(Button button) {
 		button.setStyle("-fx-border-style: none; -fx-border-width: 0px; -fx-border-insets: 0; -fx-font-size:1px; -fx-background-image: url('button.jpg')");
 		Image image = new Image(getClass().getResourceAsStream("../view/button.jpg"));
 		button.setGraphic(new ImageView(image));
 	}
 
+	/**
+	 * @param button field for setting default graphic
+	 * @author Rafał Witkowski
+	 * setDefaultBackgroundAndTextForField method sets a default graphic for a field and sets empty text for that field
+	 * updates on action add and remove ships
+	 */
 	private void setDefaultBackgroundAndTextForField(Button button) {
 		button.setStyle("-fx-border-style: none; -fx-border-width: 0px; -fx-border-insets: 0; -fx-font-size:1px; -fx-background-image: url('button.jpg')");
 		Image image = new Image(getClass().getResourceAsStream("../view/button.jpg"));
@@ -526,6 +705,13 @@ public class ControlerClient {
 		button.setGraphic(new ImageView(image));
 	}
 
+	/**
+	 * @param row row of deleted field
+	 * @param col column of deleted field
+	 * @return boolean determines if ship can be deleted
+	 * @author Rafał Witkowski
+	 * validateHorizontalDeleteButtonAction method checks if a field is a part of horizontal ship and can be deleted
+	 */
 	private boolean validateHorizontalDeleteButtonAction(Integer row, Integer col) {
 		if (col - 1 > 0) {
 			Button button = (Button) getButtonByRowColumnIndex(row, col - 1, mojGrid);
@@ -537,6 +723,13 @@ public class ControlerClient {
 		return false;
 	}
 
+	/**
+	 * @param row row of deleted field
+	 * @param col column of deleted field
+	 * @return boolean determines if ship can be deleted
+	 * @author Rafał Witkowski
+	 * validateVerticalDeleteButtonAction method checks if a field is a part of vertical ship and can be deleted
+	 */
 	private boolean validateVerticalDeleteButtonAction(Integer row, Integer col) {
 		if (row - 1 > 0) {
 			Button button = (Button) getButtonByRowColumnIndex(row - 1, col, mojGrid);
@@ -548,6 +741,11 @@ public class ControlerClient {
 		return false;
 	}
 
+	/**
+	 * @author Rafał Witkowski
+	 * checkIfShipCanBePlaced method checks if a ship can be placed in selected field
+	 * @see #validatePlacingShipForRowAndCol(Integer, Integer) ()
+	 */
 	private boolean checkIfShipCanBePlaced() {
 		Boolean isOk = true;
 		Integer row = Integer.parseInt(getRowColMoj[0]);
@@ -565,11 +763,17 @@ public class ControlerClient {
 		return isOk;
 	}
 
+	/**
+	 * @param row row of selected field
+	 * @param col column of selected field
+	 * @author Rafał Witkowski
+	 * checkIfShipCanBePlaced method checks if a ship can be placed in selected field
+	 * @see #checkIfShipCanBePlaced()
+	 */
 	private boolean validatePlacingShipForRowAndCol(Integer row, Integer col) {
 		Button button = (Button) getButtonByRowColumnIndex(row, col, mojGrid);
 		if (!button.getText().equals(""))
 			return true;
 		return false;
 	}
-
 }
--- src/application/ErrorInfoDisplay.java
+++ src/application/ErrorInfoDisplay.java
@@ -5,6 +5,12 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 
+/**
+ * This class is designed to return alerts for all actions which requires user notification<br>
+ * This class requires no params, class is public, visible for all project classes
+ *
+ * @author Rafał Witkowski
+ **/
 public class ErrorInfoDisplay {
 	public void showWrongAdressNumber() {
 		Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -14,6 +20,10 @@ public class ErrorInfoDisplay {
 		alert.showAndWait();
 	}
 
+	/**
+	 * @author Rafał Witkowski
+	 * showNoServerActive shows alert for not active server
+	 **/
 	public void showNoServerActive() {
 		Alert alert = new Alert(Alert.AlertType.ERROR);
 		alert.setHeaderText("Brak uruchomionoego serwera!");
@@ -22,6 +32,10 @@ public class ErrorInfoDisplay {
 		alert.showAndWait();
 	}
 
+	/**
+	 * @author Rafał Witkowski
+	 * showNoServerActive shows alert for not possible ship placement
+	 **/
 	public void placingShipNotPossible() {
 		Alert alert = new Alert(Alert.AlertType.ERROR);
 		alert.setHeaderText("Nie można postawić statku!");
@@ -30,61 +44,109 @@ public class ErrorInfoDisplay {
 		alert.showAndWait();
 	}
 
+	/**
+	 * @author Rafał Witkowski
+	 * enemyMissInfo shows alert of an enemy shoot miss
+	 **/
 	public void enemyMissInfo() {
 		Alert alert = new Alert(Alert.AlertType.INFORMATION, "Przeciwnik spudłował twoja kolej", ButtonType.CLOSE);
 		alert.showAndWait();
 	}
 
+	/**
+	 * @author Rafał Witkowski
+	 * youGotHitInfo shows alert of an enemy shoot hit
+	 **/
 	public void youGotHitInfo() {
 		Alert alert = new Alert(Alert.AlertType.INFORMATION, "Zostałeś trafiony, przeciwnik strzela dalej", ButtonType.CLOSE);
 		alert.showAndWait();
 	}
 
+	/**
+	 * @author Rafał Witkowski
+	 * youHitInfo shows alert of a players shoot hit
+	 **/
 	public void youHitInfo() {
 		Alert alert = new Alert(Alert.AlertType.INFORMATION, "Trafiłeś, strzelaj dalej", ButtonType.CLOSE);
 		alert.showAndWait();
 	}
 
+	/**
+	 * @author Rafał Witkowski
+	 * youMissedInfo shows alert of a players shoot missed
+	 **/
 	public void youMissedInfo() {
 		Alert alert = new Alert(Alert.AlertType.INFORMATION, "Spudłowałeś, strzela przeciwnik", ButtonType.CLOSE);
 		alert.showAndWait();
 	}
 
+	/**
+	 * @author Rafał Witkowski
+	 * pickEnemyPositionInfo shows alert of nessesity of picking a field before shooting
+	 **/
 	public void pickEnemyPositionInfo() {
 		Alert alert = new Alert(Alert.AlertType.INFORMATION, "Należy zaznaczyć pozycję okrętu przeciwnika", ButtonType.CLOSE);
 		alert.showAndWait();
 	}
 
+	/**
+	 * @author Rafał Witkowski
+	 * wrongPort shows alert of wrong port for connection
+	 **/
 	public void wrongPort() {
 		Alert alert = new Alert(Alert.AlertType.ERROR, "Nieprawidłowy port", ButtonType.CLOSE);
 		alert.showAndWait();
 	}
 
+	/**
+	 * @author Rafał Witkowski
+	 * cannotConnectToServer shows alert of not possible connection establishment
+	 **/
 	public void cannotConnectToServer() {
 		Alert alert = new Alert(Alert.AlertType.INFORMATION, "Nie można się połączyć z serverem", ButtonType.CLOSE);
 		alert.showAndWait();
 	}
 
+	/**
+	 * @author Rafał Witkowski
+	 * wrongPortOrIpAdress shows alert of wrong port or ip adress
+	 **/
 	public void wrongPortOrIpAdress() {
 		Alert alert = new Alert(Alert.AlertType.ERROR, "Nieprawidłowy port lub adres IP ", ButtonType.CLOSE);
 		alert.showAndWait();
 	}
 
+	/**
+	 * @author Rafał Witkowski
+	 * clientNotFound shows alert of client application not running
+	 **/
 	public void clientNotFound() {
 		Alert alert = new Alert(Alert.AlertType.INFORMATION, "Nie znaleziono klienta", ButtonType.CLOSE);
 		alert.showAndWait();
 	}
 
+	/**
+	 * @author Rafał Witkowski
+	 * serverAlreadyRunning shows alert of client server appliaction is already running
+	 **/
 	public void serverAlreadyRunning() {
 		Alert alert = new Alert(Alert.AlertType.INFORMATION, "Server jest już uruchomiony", ButtonType.CLOSE);
 		alert.showAndWait();
 	}
 
+	/**
+	 * @author Rafał Witkowski
+	 * serverCrashed shows alert of client server appliaction runetime error
+	 **/
 	public void serverCrashed() {
 		Alert alert = new Alert(Alert.AlertType.INFORMATION, "Server uległ awarii", ButtonType.CLOSE);
 		alert.showAndWait();
 	}
 
+	/**
+	 * @author Rafał Witkowski
+	 * showNotStartOfAShip shows alert of not picking a beginning of a ship in deleting ship action
+	 **/
 	public void showNotStartOfAShip() {
 		Alert alert = new Alert(Alert.AlertType.ERROR);
 		alert.setHeaderText("To nie jest początek statku");
@@ -93,6 +155,10 @@ public class ErrorInfoDisplay {
 		alert.showAndWait();
 	}
 
+	/**
+	 * @author Rafał Witkowski
+	 * showShipCannotBePlacedHere shows alert of not possible ship placement in picked place
+	 **/
 	public void showShipCannotBePlacedHere() {
 		Alert alert = new Alert(Alert.AlertType.ERROR);
 		alert.setHeaderText("Nie można tutaj postawić statku");
